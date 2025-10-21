88d1f4707adc755afb2bc6ea10796f2e32100e0e
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-10-21 06:30:57 +0600

    add comments to improve readability

--- notebooks/1_bronze.py
+++ notebooks/1_bronze.py
@@ -5,15 +5,15 @@ event_hub_namespace = "<<Namespace_hostname>>"
 event_hub_name="<<Eventhub_Name>>"  
 event_hub_conn_str = "<<Connection_string>>"
 
-
+# Configure Kafka connection options for reading from Azure Event Hubs.
 kafka_options = {
-    'kafka.bootstrap.servers': f"{event_hub_namespace}:9093",
-    'subscribe': event_hub_name,
-    'kafka.security.protocol': 'SASL_SSL',
-    'kafka.sasl.mechanism': 'PLAIN',
+    'kafka.bootstrap.servers': f"{event_hub_namespace}:9093", # Specifies the Event Hub namespace and port.
+    'subscribe': event_hub_name,    # Sets the Event Hub name to consume events from.
+    'kafka.security.protocol': 'SASL_SSL',    # Define secure communication using SASL over SSL with PLAIN mechanism.
+    'kafka.sasl.mechanism': 'PLAIN',    # Provides authentication credentials using the Event Hub connection string.
     'kafka.sasl.jaas.config': f'kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required username="$ConnectionString" password="{event_hub_conn_str}";',
-    'startingOffsets': 'latest',
-    'failOnDataLoss': 'false'
+    'startingOffsets': 'latest',    # Starts reading from the latest available offset.
+    'failOnDataLoss': 'false'    # Prevents job failure if data loss is detected (e.g., due to retention limits).
 }
 #Read stream from eventhub
 raw_df = (spark.readStream
